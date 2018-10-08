d91dfc95ee6cc2fea981734f9d0ffd31d7f95f15
kamenmi1 <miloslav.kamenik@outlook.cz>
2018-10-08 19:59:23 +0200

    Dokončení úkolu - otáčení podle osy Y

--- src/c_03_utery_16_35/main/Renderer.java
+++ src/c_03_utery_16_35/main/Renderer.java
@@ -40,13 +40,13 @@ public class Renderer {
         img.setRGB(x, y, color);
     }
     public void drawLine(int x1, int y1, int x2, int y2, int color) {
-        float k = (y2-y1)/(float)(x2-x1); //abychom dostali desetinne cislo (float)
-        float q = y1 - k*x1;
+        float k = (y2 - y1) / (float) (x2 - x1); //abychom dostali desetinne cislo (float)
+        float q = y1 - k * x1;
 
         // řídící osa X
-        if (Math.abs(k) <1) {
+        if (Math.abs(k) < 1) {
 
-            if (x1>x2) {
+            if (x1 > x2) {
                 int a = x1;
                 x1 = x2;
                 x2 = a;
@@ -54,15 +54,28 @@ public class Renderer {
                 a = y1;
                 y1 = y2;
                 y2 = a;
-        }
+            }
 
-            for(int x = x1; x <= x2; x++){
-                int y = Math.round (k*x+q);
+            for (int x = x1; x <= x2; x++) {
+                int y = Math.round(k * x + q);
                 drawPixel(x, y, color);
+            }
+        } else {
+            //končit doma // řídící osa Y
+            if (y1 > y2) {
+                int a = x1;
+                x1 = x2;
+                x2 = a;
+
+                a = y1;
+                y1 = y2;
+                y2 = a;
+            }
+
+            for (int y = y1; y <= y2; y++) {
+                int x = Math.round((y-q)/k);
+                drawPixel(x, y, color);
+
+            }
         }
-}
-        else {
-         //končit doma // řídící osa Y
-        }
-    }
-}
+    }}
